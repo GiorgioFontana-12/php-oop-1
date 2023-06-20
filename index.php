437f73726e8f14dc42066385b57b319237bf0eac
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-06-20 16:18:28 +0200

    fix actors

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php 
+class Actors{
+    public $Actor1;
+    public $Actor2;
+    public $Actor3;
+    public function __construct( $Actor1, $Actor2,  $Actor3)
+    {
+        $this->Actor1 = $Actor1;
+        $this->Actor2 = $Actor2;
+        $this->Actor3 = $Actor3;
+    }
+}
+
 
 class Movie {
     public $Name
     public $Duration
     public $Actors
-    function __construct($_Name, $_Duration, $_Actors ) {
+    function __construct($_Name, $_Duration, Actors $_Actors ) {
         $this->Name = $_Name;
         $this->Duration = $_Duration;
         $this->Actors = $_Actors;
     }
 }
 
-$Avengers = new Movie("Avengers", "180min", "Robert D.J. , Cris H. , ecc" )
+$Avengers = new Movie("Avengers", "180min", new Actors("Robert D.J.", "Cris H.", "ecc" ))
 
 ?>
